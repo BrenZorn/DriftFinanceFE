cc35ca93add01c6261a69ffc9487bbb2f7314300
BrenZorn <tacknsmack1516@gmail.com>
2025-01-05 00:57:16 -0800

    Login & Create components have been updated, along with scss updates
    Login component is connected to th api.
    Create component still has to be connected to the api.

--- src/components/Authentication/Create.js
+++ src/components/Authentication/Create.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { use } from 'react'
 import { useState } from 'react'
 
 function Create() {
@@ -6,6 +6,35 @@ function Create() {
   const [Password, setPassword] = useState('')
   const [Email, setEmail] = useState('')
   const [ConfirmPassword, setConfirmPassword] = useState('')
+  const [Error, setError] = useState()
+
+  const checkUserInput = () => {
+    if(Username == ""){
+      setError("Must enter a Username")
+      return false
+    }
+    if(Password === ConfirmPassword){
+      setError("Password must match")
+      return false
+    }
+    if(Email == ""){
+      setError("Must enter a email address")
+      return false
+    }
+    return true
+  }
+
+  const addUser = () =>{
+    if(checkUserInput == false){
+      return
+    }
+    console.log('passed check')
+    //get api rout and send the data to api so back end can deal with it
+  } 
+
+
+
+  //Display Error if hit 
   return (
     <div className='CreateCard'>
       <div className='FormCard'>
@@ -20,8 +49,8 @@ function Create() {
           <label>Confirm Password</label>
           <input type='password' onChange={(e)=>setConfirmPassword(e.target.value)} placeholder='Confirm Password'></input>
           <div className='ButtonCard'>
-            <button className='CreatAccount'>Create Account</button>
-            <button className='Login'>Login</button>
+            <button className='CreatAccount' onClick={(e)=>{e.preventDefault()}}>Create Account</button>
+            <a className='Login' href='/accounts?section=signin'>Login</a>
           </div>
         </form>
       </div>
--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,18 +1,39 @@
 import React, { useState } from 'react'
-import Nav from '../nav'
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
+
+
+
+
 
 function Login() {
-  const [Username, setUsername] = useState('')
+  const [Email, setEmail] = useState('')
   const [Password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  const userLogin = async (Email, Password)=>{ 
+    console.log(Email, Password)
+    let request = await axios.post('http://localhost:3001/Authentication/signin',{
+      data:{
+        email: Email,
+        password: Password
+      },
+    })
+    console.log(request)
+    //set jwt in cookies so we can use the jwt later as a api access token
+
+    navigate('/home')
+  }
+
   return (
     <div>
       <div>
         <form>
-          <label>Username</label>
-          <input type='text' onChange={(e)=>setUsername(e.target.value)} placeholder='Username'></input>
+          <label>Email</label>
+          <input type='text' onChange={(e)=>setEmail(e.target.value)} placeholder='Email'></input>
           <label>Password</label>
           <input type='password' onChange={(e)=>setPassword(e.target.value)} placeholder='Password'></input>
-          <button>Login</button>
+          <button onClick={(e)=>{e.preventDefault(); userLogin(Email, Password)}}>Login</button>
         </form>
       </div>
     </div>
