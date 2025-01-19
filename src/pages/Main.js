b10dcaf39beaa0e409bde67cedaf375b029cb847
BrenZorn <tacknsmack1516@gmail.com>
2025-01-18 19:59:13 -0800

    User data is kept in sync now
    after page refresh the user data is still kept in sync

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,19 +1,23 @@
 import React, { useEffect } from 'react'
 import Nav from '../components/nav'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import isCookieExpired from '../helpers/isCookieExpired'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import Cookies from 'js-cookie';
+import { addUser } from '../redux/slice/userInfo'
 
 
-//check if  jwt is expired
+//check if  jwt is expired ~
 //get user info 
-    //check if we have current user info
+    //check if we have current user info ~
     //if not use jwt to get current user cred from BE
 
 
 function Main() {
     const {user} = useSelector(state=>state.user)
     const navigate = useNavigate()
+    const dispatch = useDispatch()
     useEffect(()=>{
         const isTokenExpired = isCookieExpired("JWT")
         if(!isTokenExpired){
@@ -21,8 +25,13 @@ function Main() {
                 if(user.email){
                     return
                 }else{
-                    //get users data and add to redux
-                    console.log('add user to redux')  
+                    const cookieValue = Cookies.get("JWT");
+                    let cookie = JSON.parse(cookieValue)
+                    axios.get("http://localhost:3001/main/currentUser",{headers : {"Authorization" : `Bearer ${cookie.data}`}})
+                    .then(res => {
+                        dispatch(addUser(res.data))
+                    })
+                    .catch(error => {console.log(error)})
                 }
                 
             }catch(err){
@@ -38,7 +47,7 @@ function Main() {
     <div>
         <Nav />
         <div>
-            <h1>Welcome Back {user.username}</h1>
+            <h1>Welcome Back {user.UserName}</h1>
         </div>
         <div>
             accounts
